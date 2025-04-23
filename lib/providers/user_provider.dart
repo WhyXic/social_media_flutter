b918494604efa1dd40f369c38ec767ee0dc1c9a7
Vedant <99191727+WhyXic@users.noreply.github.com>
2025-04-23 06:58:43 +0530

    Update user_provider.dart

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone/models/user.dart';
 import 'package:instagram_clone/resources/auth_methods.dart';
 
-class UserProvider with ChangeNotifier {
+class UserProvider with ChangeNotifier { // make a new class called UserProvider, that gets the user details and notifies listeners if the user is updated, 
+  // So that the state of the app is globally managed.
   User? _user;
   final AuthMethods _authMethods = AuthMethods();
-  User get getUser => _user!;
+  User get getUser => _user!; // no idea.
 
   Future<void> refreshUser() async {
-    User user = await _authMethods.getUserDetails();
+    User user = await _authMethods.getUserDetails(); // gets user details
     _user = user;
-    notifyListeners();
+    notifyListeners(); // notifies the children who are listening to the provider.
   }
 }
