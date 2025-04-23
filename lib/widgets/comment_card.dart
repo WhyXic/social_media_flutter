632f6f3f4ad4e13438f6f92ee4d0b84021b0155e
Vedant <99191727+WhyXic@users.noreply.github.com>
2025-04-23 06:52:57 +0530

    Update comment_card.dart

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/providers/user_provider.dart'; // import the user provider made in the app. 
 import 'package:instagram_clone/models/user.dart';
-import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
+import 'package:intl/intl.dart'; // importing the interationalization package to convert time to other user readable format.
+import 'package:provider/provider.dart'; // import the provider package. 
 
-class CommentCard extends StatefulWidget {
+class CommentCard extends StatefulWidget {  // Make comment card a stateful widget. 
   const CommentCard({super.key, required this.snap});
   final snap;
   @override
-  State<CommentCard> createState() => _CommentCardState();
+  State<CommentCard> createState() => _CommentCardState(); // the state of CommentCard is _CommentCardSate();
 }
 
-class _CommentCardState extends State<CommentCard> {
-  @override
+class _CommentCardState extends State<CommentCard> { // make the commentcardstate an class that inherits the state with the type of that state being comment card.
+  @override // override default state build method.
   Widget build(BuildContext context) {
-    final User user = Provider.of<UserProvider>(context).getUser;
+    final User user = Provider.of<UserProvider>(context).getUser; // get the current state of user from the provider.
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 18, horizontal: 16),
+      padding: EdgeInsets.symmetric(vertical: 18, horizontal: 16), // padding to make it pretty.
       child: Row(
         children: [
           CircleAvatar(
-            backgroundImage: NetworkImage(widget.snap['profilePic']),
+            backgroundImage: NetworkImage(widget.snap['profilePic']), // displaying the profile picture from firebase storage.
             radius: 18,
           ),
           Expanded(
@@ -35,17 +35,17 @@ class _CommentCardState extends State<CommentCard> {
                     RichText(
                         text: TextSpan(children: [
                       TextSpan(
-                          text: '${widget.snap['name']}',
+                          text: '${widget.snap['name']}', // display the name
                           style: const TextStyle(fontWeight: FontWeight.bold)),
                       TextSpan(
                           text: '   ${widget.snap['text']}',
-                          style: const TextStyle())
+                          style: const TextStyle()) // display the comment. 
                     ])),
                     Padding(
                       padding: EdgeInsets.only(top: 4),
                       child: Text(
                         DateFormat.yMMMd()
-                            .format(widget.snap['datePublished'].toDate())
+                            .format(widget.snap['datePublished'].toDate()) // dispaly the datepublished. 
                             .toString(),
                         style: TextStyle(
                             fontSize: 12, fontWeight: FontWeight.w400),
@@ -56,6 +56,10 @@ class _CommentCardState extends State<CommentCard> {
               ),
             ),
           ),
+          /*
+          Not implemented the replies to comments and likes as it was similar to linking a post, where if the user has liked it, add user to the liked list, display the length - 1 
+          for the no of likes, if uid is present in the list, then just remove it if interacted again.
+          */
           Container(
             padding: EdgeInsets.all(8),
             child: IconButton(
